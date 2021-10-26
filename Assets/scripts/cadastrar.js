0a2c7611d58f3950bc3687933870a2a74f547c08
Lucas Vihuchi <lucasvihuchi@gmail.com>
2021-10-26 12:53:10 -0300

    Pequenos ajustes no layout / Funções JS do página de login e cadastro implementadas

---
+++ Assets/scripts/cadastrar.js
@@ -0,0 +1,63 @@
+"use strict";
+// 3 tipos de variáveis em JS:
+// var - Variável global (Acessada de qualquer lugar, mesmo fora da propria função)
+// let - Variável local (Acessada só no escopo onde foi declarada)
+// const - Constante e escopo local
+
+// == -> Verifica se os valores são iguais
+// != -> Verficia se os valores são diferentes
+// === -> Verfica se o valor e o tipo do valor são iguais
+// !== -> Verifica se o valor e o tipo do valor são diferentes
+
+function cadastraUsuario(event) {
+    event.preventDefault();
+    let senha = document.getElementById("Crie-sua-senha").value;
+    let confirmacaoSenha = document.getElementById("Confirme-sua-senha").value;
+
+    if (senha !== confirmacaoSenha) {
+        alert("As senhas não coincidem!");
+        return;
+    }
+    if (!(document.getElementById("Termos-de-uso").checked)) {
+        alert("Os termos de uso devem ser aceitos para continuar o cadastro!");
+        return;
+    }
+
+    let email = document.getElementById("E-mail").value;
+    if (isCadastrado(email)) {
+        alert("Email já cadastrado no sistema!");
+        return;
+    }
+
+    let usuario = {
+        nome: document.getElementById("Nome-completo").value,
+        data_nascimento: document.getElementById("Data-de-nascimento").value,
+        telefone: document.getElementById("Tel-ou-Celular").value,
+        email: email,
+        senha: senha,
+        quer_receber_emails: document.getElementById("autorizo").checked
+    }
+
+    let cadastros = JSON.parse(localStorage.getItem("usuariosCadastrados") || "[]");
+    
+    cadastros.push(usuario);
+    localStorage.setItem("usuariosCadastrados", JSON.stringify(cadastros));
+    
+    window.location.reload();
+}
+
+function isCadastrado(email) {
+    let cadastros = JSON.parse(localStorage.getItem("usuariosCadastrados"));
+    if (cadastros === null || cadastros === undefined) {
+        return false;
+    }
+    if (cadastros.length === 0) {
+        return false;
+    }
+    for (let indice = 0; indice < cadastros.length; indice++) {
+        if (email === cadastros[indice].email) {
+            return true;
+        }
+    }
+    return false;
+}
